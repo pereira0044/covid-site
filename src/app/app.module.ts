cea7e876a5cc7fa6133474031dd1e3f53df75c3f
pereira0044 <gabri.pereira.hotmail.com@gmail.com>
2022-03-27 21:23:32 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { PrimengModule } from './primeng.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PrimengModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
